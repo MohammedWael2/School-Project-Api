176cb8276eaad6ccc91fe1b72c5be727d8c74211
Mohamed Wael <mohamedweal26160@gmail.com>
2024-08-30 02:15:57 +0300

    Add files via upload

---
+++ SchoolProject.Core/Features/Department/Queries/Models/GetDepartmentByIDQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SchoolProject.Core.Bases;
+using SchoolProject.Core.Features.Department.Queries.Results;
+
+namespace SchoolProject.Core.Features.Department.Queries.Models
+{
+    public class GetDepartmentByIDQuery : IRequest<Response<GetDepartmentByIDResponse>>
+    {
+        public int Id { get; set; }
+        public GetDepartmentByIDQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ SchoolProject.Core/Features/Students/Queries/Models/GetStudentListQuery.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Core;
+using SchoolProject.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using SchoolProject.Core.Features.Students.Queries.Results;
+using SchoolProject.Core.Bases;
+
+
+namespace SchoolProject.Core.Features.Students.Queries.Models
+{
+    public class GetStudentListQuery : IRequest<Response< List<GetStudentListResponse>>>
+    {
+    }
+}
